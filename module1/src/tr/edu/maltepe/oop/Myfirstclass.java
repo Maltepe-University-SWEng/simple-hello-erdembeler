2ff906429d870b7076a5f2c002a2427e5c6fe67b
erdembeler <62485734+erdembeler@users.noreply.github.com>
2024-10-08 10:21:45 +0300

    Initial commit

--- module1/src/tr/edu/maltepe/oop/Myfirstclass.java
+++ module1/src/tr/edu/maltepe/oop/Myfirstclass.java
@@ -1,13 +1,6 @@
 package tr.edu.maltepe.oop;
 
 public class Myfirstclass {
-package tr.edu.maltepe.oop;
-public  class imple_main {
 
- public static void main(String []args){
-        //
-       System.out.println("simple main.....");
-        }
-}
 
 }
