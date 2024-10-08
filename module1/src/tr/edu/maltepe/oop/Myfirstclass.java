2d2b6444a6307d4160863d97a3cc50ca1c1f7070
erdembeler <62485734+erdembeler@users.noreply.github.com>
2024-10-08 09:42:23 +0300

    Update Myfirstclass.java
    updated

--- module1/src/tr/edu/maltepe/oop/Myfirstclass.java
+++ module1/src/tr/edu/maltepe/oop/Myfirstclass.java
@@ -1,6 +1,13 @@
 package tr.edu.maltepe.oop;
 
 public class Myfirstclass {
+package tr.edu.maltepe.oop;
+public  class imple_main {
 
+ public static void main(String []args){
+        //
+       System.out.println("simple main.....");
+        }
+}
 
 }
